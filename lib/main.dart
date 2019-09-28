fc7ccf0a1548c0711aaf38636634081ed86a8123
Peeyush <praj@teramatrix.in>
2019-09-28 15:08:17 +0530

    Flexible view for home screen - awesome

--- lib/main.dart
+++ lib/main.dart
@@ -23,26 +23,20 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.black87,
             scaffoldBackgroundColor: Colors.white,
             textTheme: ThemeData.light().textTheme.copyWith(
-              title: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              ),
-              subtitle: TextStyle(
-                color: Colors.black54,
-                fontWeight: FontWeight.normal,
-                fontSize: 16
-              ),
-            ),
+                  title: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                  subtitle: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 16),
+                ),
             appBarTheme: AppBarTheme(
               color: Colors.white,
               elevation: 0,
               textTheme: TextTheme(
-                  title: TextStyle(
-                    color: Colors.black87, 
-                    fontSize: 26
-                    )
-                  ),
+                  title: TextStyle(color: Colors.black87, fontSize: 26)),
             )));
   }
 }
@@ -66,31 +60,29 @@ class _MyHomePageState extends State<MyHomePage> {
     print(DateTime.now());
   }
 
-  // final List<Transaction> _txn = [] 
+  // final List<Transaction> _txn = []
   final List<Transaction> _txn = [
-    Transaction(id: '1', title: 'Paytm', amount: 400.0, date: DateTime(2019, 09, 23)),
-    Transaction(id: '2', title: 'Uber', amount: 100.0, date: DateTime(2019, 09, 24)),
-    Transaction(id: '3', title: 'ICIC', amount: 100.0, date: DateTime(2019, 09, 25)),
-    Transaction(id: '4', title: 'Zomato', amount: 100.0, date: DateTime(2019, 09, 25)),
-    Transaction(id: '5', title: 'Amazon', amount: 100.0, date: DateTime(2019, 09, 26)),
-    Transaction(id: '6', title: 'AWS', amount: 100.0, date: DateTime(2019, 09, 26)),
+    Transaction(
+        id: '1', title: 'Paytm', amount: 400.0, date: DateTime(2019, 09, 23)),
+    Transaction(
+        id: '2', title: 'Uber', amount: 100.0, date: DateTime(2019, 09, 24)),
+    Transaction(
+        id: '3', title: 'ICIC', amount: 100.0, date: DateTime(2019, 09, 25)),
+    Transaction(
+        id: '4', title: 'Zomato', amount: 100.0, date: DateTime(2019, 09, 25)),
+    Transaction(
+        id: '5', title: 'Amazon', amount: 100.0, date: DateTime(2019, 09, 26)),
+    Transaction(
+        id: '6', title: 'AWS', amount: 100.0, date: DateTime(2019, 09, 26)),
     Transaction(id: '7', title: 'Azure', amount: 100.0, date: DateTime.now()),
     Transaction(id: '8', title: 'Laptop', amount: 100.0, date: DateTime.now()),
     Transaction(id: '9', title: 'GPU', amount: 100.0, date: DateTime.now()),
     Transaction(id: '9', title: 'Desktop', amount: 100.0, date: DateTime.now()),
-  ]
-  ;
-
-  
+  ];
 
   List<Transaction> get _recentTransactions {
     return this._txn.where((t) {
-      return 
-      t
-      .date
-      .isAfter(
-        DateTime.now()
-        .subtract(Duration(days: 7)));
+      return t.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
     }).toList();
   }
 
@@ -122,12 +114,23 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Chart(this._recentTransactions),
-          TransactionList(this._txn),
-        ],
-      ),
+      body: Flex(
+        direction: Axis.vertical,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Chart(this._recentTransactions)
+            ),
+            Flexible(
+              flex: 3,
+              fit: FlexFit.tight,
+              child: TransactionList(this._txn)
+            ),
+          ],
+        ),
     );
   }
 }
