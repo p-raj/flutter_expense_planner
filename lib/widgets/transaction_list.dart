b1e83eed355feb326d96517c81f3f665a1394891
Peeyush <praj@teramatrix.in>
2019-09-30 13:27:57 +0530

    replacing code with the tute

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,7 +12,8 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 450,
+      height: MediaQuery.of(context).size.height * 0.6,
+      // width: MediaQuery.of(context).size.width,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
